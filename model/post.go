174a96b617a9d843e0e1babbfacf63346217ea10
Alex Gamas <alex.santos@solutis.com.br>
2018-10-19 16:42:09 -0300

    POST, GET, DELETE

--- model/post.go
+++ model/post.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"go-postbar/logger"
 	"time"
 )
 
@@ -12,9 +11,4 @@ type Post struct {
 	Title   string    `json:"title,omitempty"`
 	Message string    `json:"message,omitempty"`
 	Created time.Time `json:"created,omitempty"`
-}
-
-// Like Adiciona um like
-func (p Post) Like() {
-	logger.Log.Debug("%s", p)
 }
